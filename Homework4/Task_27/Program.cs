b613d48922a43d4d056b97ae215e4440f3fd5b86
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-23 19:48:28 +0300

    added Task 27

---
+++ Homework4/Task_27/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 27. Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+int numberA = UserNumber("Введите число: ");
+
+int result = Method1(numberA);
+
+Console.WriteLine($"{numberA} -> {result}");
+
+//=============================Методы==================
+
+//------------------------Суммирование цифр
+int Method1(int number)
+{
+    if(number<0) 
+        number=number*(-1);
+
+    int sum = number%10;
+
+    while(number!=0)
+    {
+        number=number/10;
+        sum = sum+number%10;
+    }
+return sum;
+}
+
+
+
+//----------------Ввод данных
+static int UserNumber(string messageToUser)
+{
+    Console.Write(messageToUser);
+    try
+    {
+        return int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
