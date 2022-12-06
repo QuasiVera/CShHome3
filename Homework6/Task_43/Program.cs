41ea8309e3de8daa557bb28e154584f6a7b633d3
VeraBu <Opilki-in-the-head@mail.ru>
2022-12-06 16:57:26 +0300

    modified task 43, added tuple

--- Homework6/Task_43/Program.cs
+++ Homework6/Task_43/Program.cs
@@ -6,17 +6,29 @@ double b1 = UserInput("Введите b1: ");
 double k2 = UserInput("Введите k2: ");
 double b2 = UserInput("Введите b2: ");
 
-string result = CrossPoint(k1, b1, k2, b2);
+string result = string.Empty;
 
-Console.Write($"k1 = {k1}, b1 = {b1}, k2 = {k2}, b2 = {b2} - > {result}");
+if (k1 != k2)
+{
+        (double, double) coordinate = CrossPoint(k1, b1, k2, b2);
+        result = $"k1 = {k1}, b1 = {b1}, k2 = {k2}, b2 = {b2} - > {coordinate}";
+}
+else result = $"k1 = {k1}, b1 = {b1}, k2 = {k2}, b2 = {b2} - > прямые параллельны или совпадают";
+
+Console.Write(result);
+
 
 
 //----------------Расчет точки пересечения прямых
-string CrossPoint(double k1, double b1, double k2, double b2)
+
+(double x, double y) CrossPoint(double k1, double b1, double k2, double b2)
 {
-    double x = Math.Round(((b2-b1)/(k1-k2)),3);
-    double y = Math.Round((k1*x + b1),3);
-    return $"точка пересечения ({x}; {y})";
+
+    double x = Math.Round(((b2 - b1) / (k1 - k2)), 3);
+    double y = Math.Round((k1 * x + b1), 3);
+    //return $"точка пересечения ({x}; {y})";
+    var coord = (x, y);
+    return coord;
 }
 
 
